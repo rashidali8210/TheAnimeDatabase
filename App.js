3d75667dd40f7e2503f4f399dbccbbeb7e701f8e
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2022-04-29 17:00:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import Uppy from '@uppy/core'
-import XHRUpload from '@uppy/xhr-upload'
-import Dashboard from '@uppy/dashboard'
 
-const uppy = new Uppy()
-  .use(Dashboard, {
-    trigger: '#select-files',
-  })
-  .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' })
+AvatarPicker(currentAvatar ){
+  const {user} = this.props
+  const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+             username: user.username
+        })
+    };
 
-uppy.on('complete', (result) => {
-  console.log('Upload complete! We’ve uploaded these files:', result.successful)
-  
-  
+  const uppy = Uppy({ autoProceed: true })
+      .use(Dashboard, { trigger: '#select-files' })
+      .use(GoogleDrive, { target: Dashboard, serverUrl: apiConstants.API_GENERAL_URL+'changeprofilepic' })
+      .use(Instagram, { target: Dashboard, serverUrl: apiConstants.API_GENERAL_URL+'changeprofilepic' })
+      .use(Webcam, { target: Dashboard })
+      .use(Tus, { endpoint:  apiConstants.API_GENERAL_URL+'changeprofilepic?access-token='+user.username, requestOptions })
+      .on('complete', (result) => {
+        console.log('Upload result:', result)
+      })
+
+return (
+  <div>
+    <img src={currentAvatar} alt="Current Avatar" />
+    <DragDrop
+      uppy={uppy}
+      locale={{
+        strings: {
+          // Text to show on the droppable area.
+          // `%{browse}` is replaced with a link that opens the system file selection dialog.
+          dropHereOr: 'Drop here or %{browse}',
+          // Used as the label for the link that opens the system file selection dialog.
+          browse: 'browse'
+        }
