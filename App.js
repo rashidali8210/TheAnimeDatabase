20a2405cb4f11b3536b1d5726ccf70a52869e482
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2022-04-29 16:49:41 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import Uppy from '@uppy/core'
+import XHRUpload from '@uppy/xhr-upload'
+import Dashboard from '@uppy/dashboard'
+
+const uppy = new Uppy()
+  .use(Dashboard, {
+    trigger: '#select-files',
+  })
+  .use(XHRUpload, { endpoint: 'https://api2.transloadit.com' })
+
+uppy.on('complete', (result) => {
+  console.log('Upload complete! We’ve uploaded these files:', result.successful)
+  
+  export default App; 
