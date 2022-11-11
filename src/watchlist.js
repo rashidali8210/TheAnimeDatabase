51d057de8b2d05b082ef296714c384c58902c666
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2022-11-12 00:39:15 +0530

    Create watchlist.js

---
+++ src/watchlist.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Watchlist=(props)=>{
+    return(
+        <>
+        
+        hiii
+        {/* <img src="" */}
+        {props.state2}
+        </>
+    )
+}
+export default Watchlist
